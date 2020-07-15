4e928376af0ca37dfe935580eec9908b0b31793f
Arthur <Strevitz@github.com>
2020-07-15 13:55:45 +0200

    commit

---
+++ docs/precache-manifest.6e5efd34512e22c9c643260831dd2adc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d755881d358a21b4d15a4029c26a185f",
+    "url": "/currency-convert-app/index.html"
+  },
+  {
+    "revision": "1f7e150e396f2b64c95b",
+    "url": "/currency-convert-app/static/css/main.9ca13efb.chunk.css"
+  },
+  {
+    "revision": "74a866be2896f07e2eca",
+    "url": "/currency-convert-app/static/js/2.d17c4418.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-convert-app/static/js/2.d17c4418.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f7e150e396f2b64c95b",
+    "url": "/currency-convert-app/static/js/main.7bcdab37.chunk.js"
+  },
+  {
+    "revision": "f9df173b90ae649b5b51",
+    "url": "/currency-convert-app/static/js/runtime-main.85fefc40.js"
+  }
+]);
